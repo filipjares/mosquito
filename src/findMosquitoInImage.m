ca8a9017a76b791091c813abf19ec8a4d8dcc1b4
Filip Jares <filipjares@post.cz>
2011-04-12 20:42:48 +0200

    change comments in findMosquitoInImage
    - update some comments
    - remove some commented-out code
    - remove obsolete TODO

--- src/findMosquitoInImage.m
+++ src/findMosquitoInImage.m
@@ -3,28 +3,21 @@ function [x, y] = findMosquitoInImage(img)
     % based on util_findlaser.m from the Image Acqustion Toolbox Laser tracking demo
 
     grayscale = uint8((uint16(img(:,:,1)) + uint16(img(:,:,2)) + uint16(img(:,:,3)))/3);
-
-    %threshold = 1.25 * double(min(grayscale(:)));
-    %candidates = (grayscale <= threshold);
     candidates = (grayscale < 100);
-    
     props = regionprops(bwlabel(candidates), 'Area', 'Centroid', 'MajorAxisLength');
     
     area = [props.Area];
     majorAxis = [props.MajorAxisLength];
-    area(area > 5000 | majorAxis > 100) = 0; % max area of the mosquito is about 3000
+    area(area > 5000 | majorAxis > 100) = 0; % max area of the mosquito of size 50 is about 3000
     [num_pixels, index] = max(area);
-    
-    % TODO: to avoid recognizing border instead of mosquito,
-    % check maximum mosquito area
     
     threshold = 5;
     if (num_pixels > threshold)
-        % Area of laser is big enough, get region info.
+        % Area that is big enough exists
         x = props(index).Centroid(1);
         y = props(index).Centroid(2);
     else 
-        % Area of laser is too small to get past the threshold.
+        % No mosquito found in the image
         x = NaN;
         y = NaN;
     end
